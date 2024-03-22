3a80cfa65783e29d9e3325878a4983c89b3e7f4b
NIKHIL NEGI <neginikhilsingh6@gmail.com>
2024-03-22 20:53:41 +0530

    about Primitive and Non Primitive datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,28 +1,26 @@
 // Primitive data-types
 // 7 types: Number, String, Boolean, Undefined, Null, Symbol, BigInt
-const score = 100;
-const scoreValue = 100.5;
+const score = 100; //number
+const scoreValue = 100.5; //number
 
-const isLoggedIn = false;
-const outsideTemp = null;
+const isLoggedIn = false; //boolean
+const outsideTemp = null; //object
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123') //symbol
+const anotherId = Symbol('123') //symbol
 console.log(id === anotherId) // false
 
-const bigIntValue = 1234567890123456789012345678901234567890n
+const bigIntValue = 1234567890123456789012345678901234567890n //bigint
 
 // Reference data-types(Non-Primitive)
 // 3 types: Object, Array, Function
 
-const heroes = ["shaktimaan", "naagraj", "doga"]
+const heroes = ["shaktimaan", "naagraj", "doga"] //object
 let myObj = {
     name: "nikhil",
     age: 21
-}
+}   //object
 const myFunction = function() {
     console.log("Hello World")
-}
-
-console.log(typeof myFunction)
+}   //function
