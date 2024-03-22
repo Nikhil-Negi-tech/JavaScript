3e817f00e1b975a0bea1edc37dbcf8c5fab73811
NIKHIL NEGI <neginikhilsingh6@gmail.com>
2024-03-22 08:32:08 +0530

    Add data types and test files

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser 
+console.log(3
+    +
+    3) // code readability should be good
+
+// Data Types
+let name = "Nikhil" // string
+let age = 21 // number
+let isMale = true // boolean
+let state = null // null
+
+// number => 2 to power 53
+// bigint => 2 to power 53 to 2 to power 64
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but not assigned
+// symbol => unique value
+// object => key-value pair
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
