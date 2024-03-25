c362a7f53649545e8a3e27f002ad2e29b07ce2c3
NIKHIL NEGI <neginikhilsingh6@gmail.com>
2024-03-25 08:53:34 +0530

    Add functions and object handling code

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,4 +29,36 @@ function loginUserMessage(username){
     return "Please provide a username"
 }
 // console.log(loginUserMessage("Nikhil"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1,val2,...num1){ 
+    // rest operator is used to pass multiple arguments
+    return num1
+}
+// console.log(calculateCartPrice(10,20,30)),
+
+const user = {
+    name: "nikhil",
+    age: 22,
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.age}`)
+}
+//NOTE: always check type safety of parameters used in function
+
+// handleObject(user)
+handleObject({
+    name: "nikhil",
+    age: 22,
+})
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(anyobject){
+    console.log(`the second value is ${anyobject[1]}`)
+}
+returnSecondValue(myNewArray)
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
