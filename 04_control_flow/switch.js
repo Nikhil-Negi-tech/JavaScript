40236bd310a01857786b326677a5a7e7daacca3f
NIKHIL NEGI <neginikhilsingh6@gmail.com>
2024-03-28 06:18:27 +0530

    Add switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 3
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    default:
+        console.log('Invalid Month');
+        break;
+}
