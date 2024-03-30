1ab28927510889dcb788939e509f8f8b83a3f8d5
NIKHIL NEGI <neginikhilsingh6@gmail.com>
2024-03-30 20:46:08 +0530

    Add for-of loop and map iteration examples

---
+++ 05_iterations/03_for-of_loop.js
@@ -0,0 +1,33 @@
+// for of 
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+    // console.log(num)
+}
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    // console.log(`Each character is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('UK', "United Kingdom")
+map.set('JP', "Japan")
+// console.log(map)
+for (const [key,value] of map) {
+    // console.log(`Key is ${key} and value is ${value}`)
+}
+
+const myObj = {
+    'game1': 'NFS',
+    'game2': 'FIFA',
+    'game3': 'COD'
+}
+// for (const [key,value] of myObj) {
+    // console.log(`Key is ${key} and value is ${value}`)
+// }
